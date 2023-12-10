be802bca1fbda4f056ae16f9930c4b421652cc52
DKonst21 <104372191+DKonst21@users.noreply.github.com>
2023-12-10 16:57:22 +0300

    Added phonenumber field

--- foodcartapp/serializers.py
+++ foodcartapp/serializers.py
@@ -14,14 +14,13 @@ class OrderDetailsSerializer(ModelSerializer):
     def create(self, validated_data):
         products_data = validated_data.pop('products')
         order = OrderDetails.objects.create(**validated_data)
-        phonenumber = OrderDetails.objects.create(phonenumber=validated_data['phonenumber'])
         for product_data in products_data:
             OrderedProducts.objects.create(
                 order=order,
                 price=product_data['product'].price,
-                phonenumber=phonenumber,
                 **product_data
             )
+            OrderDetails.objects.create(phonenumber=validated_data['phonenumber'])
         return order
 
     class Meta:
